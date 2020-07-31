5aea6601b56f4a779ccb5557ea901b4159ff89a2
dekarst <dekarsts@gmail.com>
2020-07-30 22:08:07 -0400

    init commit

---
+++ public/js/app.js
@@ -0,0 +1,14 @@
+angular.module('sampleApp',
+	[
+		'ngRoute',
+		'appRoutes',
+		'MainCtrl',
+		'AddEventCtrl',
+		'AddEventService',
+		'LogInCtrl',
+		'LogInService',
+		'md.data.table',
+		'ngMaterial',
+		'mdPickers'
+	]
+);
